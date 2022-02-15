40e3f071b33ab5b51f6e71d5bbf774176453ec73
Logician <logician666@outlook.com>
2022-02-15 23:05:35 +0200

    Ignore the test.py file.

--- Languages/Python/PCPP/OOP/test.py
+++ Languages/Python/PCPP/OOP/test.py
@@ -1,16 +1,30 @@
-def simple_decorator(own_function):
+class Delicacy:
+    def __init__(self, name, price, weight):
+        self.name, self.price, self.weight = name, price, weight
+    
+    def __str__(self):
+        return f"{self.name} candy weighting {self.weight} units and costing {self.price} per unit."
 
-    def internal_wrapper(*args, **kwargs):
-        print('"{}" was called with the following arguments'.format(own_function.__name__))
-        print('\t{}\n\t{}\n'.format(args, kwargs))
-        own_function(*args, **kwargs)
-        print('Decorator is still operating')
+warehouse = list()
+warehouse.append(Delicacy(name='Lolly Pop', price=0.4, weight=133))
+warehouse.append(Delicacy(name='Licorice', price=0.1, weight=251))
+warehouse.append(Delicacy(name='Chocolate', price=1, weight=601))
+warehouse.append(Delicacy(name='Sours', price=0.01, weight=513))
+warehouse.append(Delicacy(name='Hard candies', price=0.3, weight=433))
 
-    return internal_wrapper
+import copy
 
+new_warehouse = copy.deepcopy(warehouse)
+for item in new_warehouse:
+    if item.weight > 300:
+        item.price *= 0.8
 
-@simple_decorator
-def combiner(*args, **kwargs):
-    print("\tHello from the decorated function; received arguments:", args, kwargs)
+print('*'*20)
+print('Source list of candies')
+for item in warehouse:
+    print(item)
 
-# combiner('a', 'b', exec='yes')
+print('*'*20)
+print('Price proposal')
+for item in new_warehouse:
+    print(item)
